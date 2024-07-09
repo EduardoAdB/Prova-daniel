aa7933f9a4c08a328f07f010fe0388e5ea029104
Gabriel Pedro <pkzinhogabriel@gmail.com>
2024-07-09 13:46:30 -0300

    Criação das Variavéis e Método

--- Prova Daniel dia 9 do 7/Assets/script/GameManager.cs
+++ Prova Daniel dia 9 do 7/Assets/script/GameManager.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    int pontos, teclaAtual;
+    float relogio;
+    KeyCode teclas;
+
+    
     // Start is called before the first frame update
+    void GerarSetas()
+    {
+
+    }
     void Start()
     {
-        
+        GerarSetas();
     }
 
     // Update is called once per frame

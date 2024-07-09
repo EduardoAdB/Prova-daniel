563edb55ed8ae8535a158ebdfa763570c86dc9b6
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-07-09 14:32:01 -0300

    atualizarSeta feito
    ja to querendo desistir

--- Prova Daniel dia 9 do 7/Assets/script/UiManager.cs
+++ Prova Daniel dia 9 do 7/Assets/script/UiManager.cs
@@ -26,7 +26,29 @@ public class UiManager : MonoBehaviour
     {
         for (int i = 0; i < imagens.Length; i++)
         {
-            sprite = ;
+            if (i >= setas.Length)
+            { 
+                imagens[i].sprite = sprite[0];
+            }
+            else if (setas[i] == KeyCode.DownArrow)
+            {
+                imagens[i].sprite = sprite[1];
+            }
+            else if (setas[i] == KeyCode.UpArrow)
+            {
+                imagens[i].sprite = sprite[2];
+            }
+            else if (setas[i] == KeyCode.LeftArrow)
+            {
+                imagens[i].sprite = sprite[3];
+            }
+            else if (setas[i] == KeyCode.RightArrow)
+            {
+                imagens[i].sprite = sprite[4];
+            }
+
+            imagens[i].color = Color.white;
         }
     }
+    
 }

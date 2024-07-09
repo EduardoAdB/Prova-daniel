728118ec851e174ade15edc8079eb755440f0fc1
Gabriel Pedro <pkzinhogabriel@gmail.com>
2024-07-09 13:50:02 -0300

    Criação das Variáveis e Métodos

--- Prova Daniel dia 9 do 7/Assets/script/GameManager.cs
+++ Prova Daniel dia 9 do 7/Assets/script/GameManager.cs
@@ -23,6 +23,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+
+        }
     }
 }
